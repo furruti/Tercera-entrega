743ef13fe5df5a4fe5cfc4a7b6900f1c5277f1ff
furruti <urrutifrancisco@gmail.com>
2024-09-15 18:39:54 -0300

    Update urls.py

--- terceraentrega/urls.py
+++ terceraentrega/urls.py
@@ -1,7 +1,8 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('crear_autor/', views.crear_autor, name='crear_autor'),
-    path('buscar_autor/', views.buscar_autor, name='buscar_autor'),
+    path('crear-autor/', views.crear_autor, name='crear_autor'),
+    path('buscar-autor/', views.buscar_autor, name='buscar_autor'),
 ]
