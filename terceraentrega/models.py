a447ed292e7ee3ae9f8658406870c8b389704dba
furruti <urrutifrancisco@gmail.com>
2024-09-15 18:39:32 -0300

    Update models.py

--- terceraentrega/models.py
+++ terceraentrega/models.py
@@ -2,13 +2,23 @@ from django.db import models
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=100)
-    nacionalidad = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=200)
     fecha_publicacion = models.DateField()
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
 
-class Editorial(models.Model):
-    nombre = models.CharField(max_length=100)
-    ubicacion = models.CharField(max_length=100)
+    def __str__(self):
+        return self.titulo
