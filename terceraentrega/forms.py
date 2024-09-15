d83e9b95b79f6729a889fdf28dac5d3db6227e72
furruti <urrutifrancisco@gmail.com>
2024-09-15 18:38:51 -0300

    Update forms.py

--- terceraentrega/forms.py
+++ terceraentrega/forms.py
@@ -5,13 +5,25 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'placeholder': 'Nombre del autor'}),
+            'nacionalidad': forms.TextInput(attrs={'placeholder': 'Nacionalidad del autor'}),
+        }
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
         fields = '__all__'
+        widgets = {
+            'titulo': forms.TextInput(attrs={'placeholder': 'Título del libro'}),
+            'fecha_publicacion': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 class EditorialForm(forms.ModelForm):
     class Meta:
         model = Editorial
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'placeholder': 'Nombre de la editorial'}),
+            'ubicacion': forms.TextInput(attrs={'placeholder': 'Ubicación de la editorial'}),
+        }
